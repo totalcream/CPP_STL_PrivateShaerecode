11448d8b351d94b24fa5dbd6b528a7969b71f2a9
우승훈 <mifks74@naver.com>
2020-11-06 17:24:49 +0900

    소수판단 함수 추가

--- CPP_STL_Vector/CPP_STL_Vector/소스.cpp
+++ CPP_STL_Vector/CPP_STL_Vector/소스.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
 
-#include "헤더.h"
-
 #define USER_MAX 6
 
 using namespace std;
@@ -13,6 +11,39 @@ struct UserInfo
 	int Level;
 	int Exp;
 };
+
+void find_primeNumber(int target)
+{
+	bool* arr = new bool[target + 1](); // 숫자를 지울 배열
+
+	arr[0] = true;
+	arr[1] = true;
+
+	// 2부터 특정 수의 배수에 해당하는 수를 모두 지움
+	for (int i = 2; i <= target; i++)
+	{
+		if (arr[i]) 
+			continue; // 이미 지워진 수라면 건너뜀
+
+		// 이미 지워진 숫자가 아니라면, 해당 숫자의 배수를 모두 true로 만듦
+		for (int j = 2 * i; j <= target; j += i)
+		{
+			arr[j] = true;
+		}
+	}
+
+	// 남아있는 수를 모두 출력 (배열에서 false인 index) 
+	for (int i = 2; i <= target; i++)
+	{
+		if (!arr[i])
+		{
+			cout << i << " ";
+		}
+
+	}
+
+	delete[] arr;
+}
 
 //게임 방의 유저를 관리하는 클래스
 //방에는 최대 6명까지 들어 갈 수 있다.
@@ -115,5 +146,6 @@ GameRoomUser::~GameRoomUser()
 
 int main()
 {
+	find_primeNumber(10000);
 	return 0;
 }
